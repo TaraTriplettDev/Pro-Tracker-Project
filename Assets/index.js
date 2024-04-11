15ed0598e8c2f87e229e0f388e6d582bc2915be6
Vguffin <135627490+Vguffin@users.noreply.github.com>
2024-04-11 18:50:22 -0500

    Update index.js

---
+++ Assets/index.js
@@ -0,0 +1,23 @@
+<script>
+
+const loginKey = 
+
+loginKey(username, password){
+    userName,
+    passWord,
+};
+
+
+
+
+
+
+function windowAlert(){
+    alert("Project has been saved.");
+}
+windowAlert();
+
+<button onclick="alert('Hello')">Click me.</button>
+
+
+  </script>
